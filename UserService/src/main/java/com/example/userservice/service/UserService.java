53d2695ed80c149ec71c6b45ce734d345f15273c
@CX330_x <2810288656@qq.com>
2022-12-18 19:45:15 +0800

    SpringCloud+EurekaDemo

---
+++ UserService/src/main/java/com/example/userservice/service/UserService.java
@@ -0,0 +1,17 @@
+package com.example.userservice.service;
+
+import com.example.userservice.pojo.User;
+import com.example.userservice.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public User queryById(Long id) {
+        return userMapper.findById(id);
+    }
+}
